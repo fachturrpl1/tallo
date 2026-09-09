c1f6c842653c3c773a6dd3214a200c4d4d985a67
firstynt <fachturrpl1@gmail.com>
2026-09-09 21:19:11 +0700

    feat(lib): migrating transaksi_card

---
+++ lib/transaksi_card.dart
@@ -0,0 +1,37 @@
+import 'package:flutter/material.dart';
+import 'main.dart';
+
+class TransaksiCard extends StatelessWidget {
+  final String keterangan;
+  final String jenis;
+  final String kategori;
+  final int jumlah;
+  final String tanggal;
+
+  TransaksiCard({
+    super.key,
+    required this.keterangan,
+    required this.jenis,
+    required this.kategori,
+    required this.jumlah,
+    required this.tanggal,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      child: ListTile(
+        title: Text(keterangan),
+        subtitle: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text('Jenis: $jenis'),
+            Text('Kategori: $kategori'),
+            Text('Jumlah: $jumlah'),
+            Text('Tanggal: $tanggal'),
+          ],
+        ),
+      ),
+    );
+  }
+}
