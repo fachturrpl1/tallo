ec1908ee2ed408dc449b0b386ba2bf18cc9b3912
firstynt <fachturrpl1@gmail.com>
2026-09-14 08:11:46 +0700

    chore(main): delete kategori name parameter

--- lib/main.dart
+++ lib/main.dart
@@ -12,35 +12,35 @@ final List<Transactions> transactionsList = [
   Transactions(
     keterangan: 'Penjualan hari senin',
     masuk: true,
-    kategori: kategoriLabel('1'),
+    kategori: '1',
     jumlah: 350000,
     tanggal: Transactions.inputTanggal(10, 9, 2026),
   ),
   Transactions(
     keterangan: 'Gaji Bulanan',
     masuk: true,
-    kategori: kategoriLabel('3'),
+    kategori: '3',
     jumlah: 5000000,
     tanggal: Transactions.inputTanggal(11, 9, 2026),
   ),
   Transactions(
     keterangan: 'Belanja Bahan Makanan',
     masuk: false,
-    kategori: kategoriLabel('2'),
+    kategori: '2',
     jumlah: 150000,
     tanggal: Transactions.inputTanggal(12, 9, 2026),
   ),
   Transactions(
     keterangan: 'Penjualan hari selasa',
     masuk: false,
-    kategori: kategoriLabel('1'),
+    kategori: '1',
     jumlah: 400000,
     tanggal: Transactions.inputTanggal(13, 9, 2026),
   ),
   Transactions(
     keterangan: 'Belanja Bahan Makanan',
     masuk: false,
-    kategori: kategoriLabel('2'),
+    kategori: '2',
     jumlah: 200000,
     tanggal: Transactions.inputTanggal(14, 9, 2026),
   ),
@@ -48,7 +48,7 @@ final List<Transactions> transactionsList = [
   Transactions(
     keterangan: 'Gaji Bulanan',
     masuk: true,
-    kategori: kategoriLabel('3'),
+    kategori: '3',
     jumlah: 5000000,
     tanggal: Transactions.inputTanggal(15, 9, 2026),
   ),
@@ -56,7 +56,7 @@ final List<Transactions> transactionsList = [
   Transactions(
     keterangan: 'Belanja Bahan Makanan',
     masuk: false,
-    kategori: kategoriLabel('2'),
+    kategori: '2',
     jumlah: 250000,
     tanggal: Transactions.inputTanggal(16, 9, 2026),
   ),
@@ -64,7 +64,7 @@ final List<Transactions> transactionsList = [
   Transactions(
     keterangan: 'Penjualan hari rabu',
     masuk: false,
-    kategori: kategoriLabel('1'),
+    kategori: '1',
     jumlah: 450000,
     tanggal: Transactions.inputTanggal(17, 9, 2026),
   ),
@@ -84,16 +84,18 @@ class TalloApp extends StatelessWidget {
         ],
       ),
 
-      darkTheme: ThemeData(
-        brightness: Brightness.dark,
-        extensions: const <ThemeExtension<dynamic>>[
-          AppColors.dark,
-        ],
-      ),
+      // darkTheme: ThemeData(
+      //   brightness: Brightness.dark,
+      //   extensions: const <ThemeExtension<dynamic>>[
+      //     AppColors.dark,
+      //   ],
+      // ),
 
-      themeMode: ThemeMode.system,
+      themeMode: ThemeMode.light,
+      // themeMode: ThemeMode.dark,
+      // themeMode: ThemeMode.system,      
 
-      home: const SplashScreen()
+      home: SplashScreen(transactionsList: transactionsList),
     );
   }
-}
+} 
